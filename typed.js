da2cd755784326a9f422d3e9b902b66f753bbf5a
(◣_◢) <133505751+harveyware@users.noreply.github.com>
2023-05-14 14:39:31 +0100

    Add files via upload

--- typed.js
+++ typed.js
@@ -1,7 +1,7 @@
 new Typed('#typed', {
     strings: ['lua dev', ' c++ dev','html dev','c# dev'], 
-    typeSpeed: 100,
-    delaySpeed: 200,
+    typeSpeed: 65,
+    delaySpeed: 150,
     showCursor: true,
     loop: true
 });
